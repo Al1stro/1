b438d7159d00332e1523f0de64e15a7ecc024a8c
Al1stro <145255629+Al1stro@users.noreply.github.com>
2023-12-14 16:21:05 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+TOKEN = "6544042553:AAEl0vTe_ZVIp3axCHBj8aLC550qTliQSjM"
+
+API_KEY = "XgOWc6q3Mpjg55A3VZ8XN4QIaSd9YlAM"
+
+currencies = {
+    'Доллар': 'USD',
+    'Евро': 'EUR',
+    'Рубль': 'RUB',
+    }
